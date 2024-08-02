dadc60523ff70993299b2707bc8c15621a3c9bab
Malhar <105921273+MaxxCode8@users.noreply.github.com>
2024-08-03 03:07:57 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.app'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
